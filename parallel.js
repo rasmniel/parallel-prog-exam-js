d26c954adb06d251f7706666558611fb2aefeba2
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2017-10-08 15:10:52 +0200

    commit

--- parallel.js
+++ parallel.js
@@ -9,25 +9,42 @@ document.addEventListener('DOMContentLoaded', () => {
 	lengthInput = document.querySelector('input.length');
 	output = document.querySelector('.output');
 	runButton = document.querySelector('.run-button');
+	syncTime = document.querySelector('.sync-time span');
+	asyncTime = document.querySelector('.async-time span');
+
+	// const data = populate(countInput.value, lengthInput.value);
+	const data = populate(10000, 10000);
 
 	runButton.addEventListener('click', () => {
 		while (output.firstChild)
 			output.removeChild(output.firstChild);
 
-		const data = populate(countInput.value, lengthInput.value);
-		console.time('elapsed time');
-		const result = procedure(data);
-		console.timeEnd('elapsed time');
-		console.log(data, result);
 
-		const n = Math.min(result.length, 100);
-		for (let i = 0; i < n; i++) {
-			const element = document.createElement('p');
-			element.innerHTML = result[i];
-			output.append(element);
-		}
+		setTimeout(() => {
+			console.log('starting async')
+			const asyncElapsed = runProcedure(data);
+			asyncTime.innerHTML = asyncElapsed.toFixed(4);
+			// const n = Math.min(result.length, 100);
+			// for (let i = 0; i < n; i++) {
+			// 	const element = document.createElement('p');
+			// 	element.innerHTML = result[i];
+			// 	output.append(element);
+			// }
+		}, 0);
+		console.log('run async');
+
+		console.log('starting sync')
+		const syncElapsed = runProcedure(data);
+		syncTime.innerHTML = syncElapsed.toFixed(4);
 	});
 });
+
+const runProcedure = (data) => {
+	const start = performance.now();
+	const result = procedure(data);
+	const elapsed = performance.now() - start;
+	return elapsed;
+};
 
 const populate = (count, length) => {
 	const arrayHolder = [];
