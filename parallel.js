79d7852bcb5305a50e37b11cd614e8bdc0b77d7a
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2017-10-08 13:09:33 +0200

    commit

--- parallel.js
+++ parallel.js
@@ -1,4 +1,35 @@
-const procedure = (count, length) => {
+let
+	output,
+	countInput,
+	lengthInput,
+	runButton;
+
+document.addEventListener('DOMContentLoaded', () => {
+	countInput = document.querySelector('input.count');
+	lengthInput = document.querySelector('input.length');
+	output = document.querySelector('.output');
+	runButton = document.querySelector('.run-button');
+
+	runButton.addEventListener('click', () => {
+		while (output.firstChild)
+			output.removeChild(output.firstChild);
+
+		const data = populate(countInput.value, lengthInput.value);
+		console.time('elapsed time');
+		const result = procedure(data);
+		console.timeEnd('elapsed time');
+		console.log(data, result);
+
+		const n = Math.min(result.length, 100);
+		for (let i = 0; i < n; i++) {
+			const element = document.createElement('p');
+			element.innerHTML = result[i];
+			output.append(element);
+		}
+	});
+});
+
+const populate = (count, length) => {
 	const arrayHolder = [];
 	for (let i = 0; i < count; i++) {
 		const array = [];
@@ -8,7 +39,20 @@ const procedure = (count, length) => {
 		}
 		arrayHolder.push(array);
 	}
-
+	return arrayHolder;
 };
 
-procedure(5, 10);
+const procedure = (superArray) => {
+	const sumArray = [];
+	for (let i = 0; i < superArray.length; i++) {
+		const subArray = superArray[i];
+		for (let j = 0; j < subArray.length; j++) {
+			const value = subArray[j];
+			if (sumArray[j])
+				sumArray[j] += value;
+			else
+				sumArray[j] = value;
+		}
+	}
+	return sumArray;
+};
