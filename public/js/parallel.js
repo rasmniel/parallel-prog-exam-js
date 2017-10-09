65c4e329b290df715d325f78a41fab58bdf0d7fa
rasmniel <rasmniel@gmail.com>
2017-10-09 22:58:33 +0200

    Implemented timers and progress bar

--- public/js/parallel.js
+++ public/js/parallel.js
@@ -1,103 +1,168 @@
-let
-	output,
-	countInput,
-	lengthInput,
-	populateButton,
-	procedureButton,
-	syncTime,
-	asyncTime,
-	asyncCheck,
-	killButton;
-
-let worker;
+/*
+ On DOMContentLoaded event, initialize everything.
+ */
 
 document.addEventListener('DOMContentLoaded', () => {
-	countInput = document.querySelector('input.count');
-	lengthInput = document.querySelector('input.length');
-	output = document.querySelector('.output');
-	populateButton = document.querySelector('.populate-button');
-	procedureButton = document.querySelector('.procedure-button');
-	killButton = document.querySelector('.kill-button');
-	syncTime = document.querySelector('.sync-time span');
-	asyncTime = document.querySelector('.async-time span');
-	asyncCheck = document.querySelector('.async-check');
-
-	terminateAndCreateWorker();
-
-	worker.onmessage = function(event) {
-		console.log('Received result from worker', event.data);
-		const
-			type = event.data.type,
-			payload = event.data.payload;
-
-		switch (type) {
-			case 'populate':
-				// TODO something
-				break;
-
-			case 'procedure':
-				displayResults(payload);
-				break;
-		}
-	};
-
-	populateButton.addEventListener('click', () => {
-		const
-			count = countInput.value,
-			length = lengthInput.value;
-		if (asyncCheck.checked) {
-			worker.postMessage({
-				type: 'populate',
-				payload: {
-					count: count,
-					length: length
-				}
-			});
-		} else {
-			populate(count, length);
-		}
-	});
-
-	procedureButton.addEventListener('click', () => {
-		if (asyncCheck.checked) {
-			worker.postMessage({
-				type: 'procedure'
-			});
-		} else {
-			const results = executeProcedure();
-			displayResults(results);
-		}
-	});
-
-	killButton.addEventListener('click', () => {
-		terminateAndCreateWorker();
-	});
+    initUI();
+    initWorker();
 });
 
-const terminateAndCreateWorker = () => {
-	if (worker) {
-		worker.terminate();
-		console.log('worker terminated');
-	}
-	worker = new Worker('js/worker.js');
+
+/*
+ HTML interface initialization.
+ */
+
+let
+    countInput,
+    lengthInput,
+    populateButton,
+    procedureButton,
+    progressBar,
+    killButton,
+    asyncCheck,
+    syncPopulateTime,
+    syncProcedureTime,
+    asyncPopulateTime,
+    asyncProcedureTime,
+    output;
+
+const initUI = () => {
+    countInput = document.querySelector('input.count');
+    lengthInput = document.querySelector('input.length');
+
+    asyncCheck = document.querySelector('.async-check');
+    asyncCheck.checked = true;
+
+    populateButton = document.querySelector('.populate-button');
+    procedureButton = document.querySelector('.procedure-button');
+
+    progressBar = document.querySelector('.progress');
+    killButton = document.querySelector('.kill-button');
+
+    syncPopulateTime = document.querySelector('.sync-time .populate span');
+    syncProcedureTime = document.querySelector('.sync-time .procedure span');
+    asyncPopulateTime = document.querySelector('.async-time .populate span');
+    asyncProcedureTime = document.querySelector('.async-time .procedure span');
+
+    output = document.querySelector('.output');
+};
+
+
+/*
+ DOM manipulation and UI animation.
+ */
+
+const setProgress = (progress, total) => {
+    const currentProgress = (progress / total) * 100;
+    progressBar.style.cssText = "width: " + currentProgress + "%;";
 };
 
 const MAX_RESULTS = 1000;
-const displayResults = (result) => {
-	while (output.firstChild)
-		output.removeChild(output.firstChild);
+const displayResults = (array) => {
+    while (output.firstChild)
+        output.removeChild(output.firstChild);
 
-	const array = result.array;
-	const n = Math.min(array.length, MAX_RESULTS);
-	for (let i = 0; i < n; i++) {
-		const element = document.createElement('p');
-		element.innerHTML = array[i];
-		output.append(element);
-	}
+    const n = Math.min(array.length, MAX_RESULTS);
+    for (let i = 0; i < n; i++) {
+        const element = document.createElement('p');
+        element.innerHTML = array[i];
+        output.append(element);
+    }
+};
 
-	const elapsed = result.time.toFixed(4);
-	if (result.async)
-		asyncTime.innerHTML = elapsed;
-	else
-		syncTime.innerHTML = elapsed;
+const displayTime = (time, async, populate) => {
+    const elapsed = time.toFixed(4);
+    if (async) {
+        if (populate)
+            asyncPopulateTime.innerHTML = elapsed;
+        else
+            asyncProcedureTime.innerHTML = elapsed;
+    }
+    else {
+        if (populate)
+            syncPopulateTime.innerHTML = elapsed;
+        else
+            syncProcedureTime.innerHTML = elapsed;
+    }
+};
+
+
+/*
+ Javascript web worker functionality.
+ */
+
+let worker = null;
+
+const initWorker = () => {
+    worker = new Worker('js/worker.js');
+
+    worker.onmessage = (event) => {
+        const
+            type = event.data.type,
+            payload = event.data.payload;
+
+        switch (type) {
+            case 'populate':
+                displayTime(payload.time, true, true);
+                break;
+
+            case 'procedure':
+                displayResults(payload.array);
+                displayTime(payload.time, true, false);
+                break;
+
+            case 'progress':
+                setProgress(payload.progress, payload.total);
+                break;
+        }
+    };
+
+    populateButton.addEventListener('click', populateAction);
+    procedureButton.addEventListener('click', procedureAction);
+    killButton.addEventListener('click', killAction);
+};
+
+const populateAction = () => {
+    setProgress(0, 1);
+    const
+        count = countInput.value,
+        length = lengthInput.value;
+
+    if (asyncCheck.checked) {
+        worker.postMessage({
+            type: 'populate',
+            payload: {
+                count: count,
+                length: length
+            }
+        });
+    }
+    else {
+        const result = executePopulate(count, length);
+        displayTime(result.time, false, true);
+    }
+};
+
+const procedureAction = () => {
+    setProgress(0, 1);
+    if (asyncCheck.checked) {
+        worker.postMessage({
+            type: 'procedure'
+        });
+    }
+    else {
+        const result = executeProcedure();
+        displayResults(result.array);
+        displayTime(result.time, false, false);
+    }
+};
+
+const killAction = () => {
+    populateButton.removeEventListener('click', populateAction);
+    procedureButton.removeEventListener('click', procedureAction);
+    killButton.removeEventListener('click', killAction);
+    worker.terminate();
+    console.log('Worker terminated');
+    worker = null;
+    initWorker();
 };
--- public/js/worker.js
+++ public/js/worker.js
@@ -1,91 +1,95 @@
 let arrayHolder = [];
-let cancelled = false;
 
-onmessage = function(event) {
-	const
-		type = event.data.type,
-		payload = event.data.payload;
+// scope.document can be used to determine whether this is actually a worker thread.
+const scope = this;
 
-	let result = {};
-	switch (type) {
-		case 'populate':
-			populate(payload.count, payload.length);
-			break;
+onmessage = function (event) {
+    const
+        type = event.data.type,
+        payload = event.data.payload;
 
-		case 'procedure':
-			result = executeProcedure();
-			break;
+    let result = {};
+    switch (type) {
+        case 'populate':
+            result = executePopulate(payload.count, payload.length);
+            break;
 
-		case 'cancel':
-			cancelled = true;
-			break;
-	}
-	postMessage({
-		type: type,
-		payload: result
-	});
+        case 'procedure':
+            result = executeProcedure();
+            break;
+    }
+    postMessage({
+        type: type,
+        payload: result
+    });
 };
 
-// TODO Make postMessage invocations for interval prints to inform interface about progress.
-const PRINT_INTERVAL = 1000;
 const populate = (count, length) => {
-	arrayHolder = [];
-	console.log('Running populate');
-	for (let i = 0; i < count; i++) {
+    arrayHolder = [];
+    console.log('Running populate');
+    for (let i = 0; i < count; i++) {
+        postProgress(i, count);
 
-		if (cancelled)
-			return cancel();
-
-		if (i > 0 && i % PRINT_INTERVAL === 0)
-			console.log('Created ' + i + ' arrays');
-
-		const array = [];
-		for (let j = 0; j < length; j++) {
-			const value = Math.random() * 9000 + 1000;
-			array.push(parseInt(value));
-		}
-		arrayHolder.push(array);
-	}
-	console.log('Populated arrays', arrayHolder);
-};
-
-const executeProcedure = () => {
-	const start = performance.now();
-	const array = procedure();
-	const elapsed = performance.now() - start;
-	return {
-		async: true,
-		array: array,
-		time: elapsed,
-	};
+        const array = [];
+        for (let j = 0; j < length; j++) {
+            const value = Math.random() * 9000 + 1000;
+            array.push(parseInt(value));
+        }
+        arrayHolder.push(array);
+    }
+    console.log('Populated arrays', arrayHolder);
 };
 
 const procedure = () => {
-	console.log('Running adding procedure');
-	const sumArray = [];
-	for (let i = 0; i < arrayHolder.length; i++) {
+    console.log('Running adding procedure');
+    const n = arrayHolder.length;
+    const sumArray = [];
+    for (let i = 0; i < n; i++) {
+        postProgress(i, n);
 
-		if (i > 0 && i % PRINT_INTERVAL === 0)
-			console.log('Accumulated ' + i + ' arrays');
-
-		const array = arrayHolder[i];
-		for (let j = 0; j < array.length; j++) {
-			const value = array[j];
-			if (sumArray[j])
-				sumArray[j] += value;
-			else
-				sumArray[j] = value;
-		}
-	}
-	console.log('Finished adding array values');
-	return sumArray;
+        const array = arrayHolder[i];
+        for (let j = 0; j < array.length; j++) {
+            const value = array[j];
+            if (sumArray[j])
+                sumArray[j] += value;
+            else
+                sumArray[j] = value;
+        }
+    }
+    console.log('Finished adding array values', sumArray);
+    return sumArray;
 };
 
-const cancel = () => {
-	postMessage({
-		type: 'cancel'
-	});
-	setTimeout(function() {
-		cancelled = false;
-	}, 1000);
+const PROGRESS_INTERVAL = 1000;
+const postProgress = (progress, total) => {
+    if (scope.document) return;
+    if (progress === total - 1 || progress % PROGRESS_INTERVAL === 0) {
+        console.log('Progress: ', progress + ' / ' + total);
+        postMessage({
+            type: 'progress',
+            payload: {
+                progress: progress,
+                total: total
+            }
+        });
+    }
+};
+
+const executePopulate = (count, length) => {
+    const start = performance.now();
+    populate(count, length);
+    const elapsed = performance.now() - start;
+    return {
+        time: elapsed,
+    };
+};
+
+const executeProcedure = () => {
+    const start = performance.now();
+    const array = procedure();
+    const elapsed = performance.now() - start;
+    return {
+        array: array,
+        time: elapsed,
+    };
 };
