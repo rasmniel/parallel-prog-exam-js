83253fd9f25202679d44938f7cb4e3779f942622
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2017-10-09 14:46:14 +0200

    added something - can't remember what

--- public/js/parallel.js
+++ public/js/parallel.js
@@ -1,73 +1,103 @@
 let
-    output,
-    countInput,
-    lengthInput,
-    populateButton,
-    procedureButton,
-    syncTime,
-    asyncTime;
+	output,
+	countInput,
+	lengthInput,
+	populateButton,
+	procedureButton,
+	syncTime,
+	asyncTime,
+	asyncCheck,
+	killButton;
+
+let worker;
 
 document.addEventListener('DOMContentLoaded', () => {
-    countInput = document.querySelector('input.count');
-    lengthInput = document.querySelector('input.length');
-    output = document.querySelector('.output');
-    populateButton = document.querySelector('.populate-button');
-    procedureButton = document.querySelector('.procedure-button');
-    syncTime = document.querySelector('.sync-time span');
-    asyncTime = document.querySelector('.async-time span');
+	countInput = document.querySelector('input.count');
+	lengthInput = document.querySelector('input.length');
+	output = document.querySelector('.output');
+	populateButton = document.querySelector('.populate-button');
+	procedureButton = document.querySelector('.procedure-button');
+	killButton = document.querySelector('.kill-button');
+	syncTime = document.querySelector('.sync-time span');
+	asyncTime = document.querySelector('.async-time span');
+	asyncCheck = document.querySelector('.async-check');
 
-    const worker = new Worker('js/worker.js');
+	terminateAndCreateWorker();
 
-    worker.onmessage = function (event) {
-        console.log('Received result from worker', event.data);
-        const
-            type = event.data.type,
-            payload = event.data.payload;
+	worker.onmessage = function(event) {
+		console.log('Received result from worker', event.data);
+		const
+			type = event.data.type,
+			payload = event.data.payload;
 
-        switch (type) {
-            case 'populate':
-                // TODO something
-                break;
+		switch (type) {
+			case 'populate':
+				// TODO something
+				break;
 
-            case 'procedure':
-                displayResults(payload);
-                break;
-        }
-    };
+			case 'procedure':
+				displayResults(payload);
+				break;
+		}
+	};
 
-    populateButton.addEventListener('click', () => {
-        worker.postMessage({
-            type: 'populate',
-            payload: {
-                count: countInput.value,
-                length: lengthInput.value
-            }
-        });
-    });
+	populateButton.addEventListener('click', () => {
+		const
+			count = countInput.value,
+			length = lengthInput.value;
+		if (asyncCheck.checked) {
+			worker.postMessage({
+				type: 'populate',
+				payload: {
+					count: count,
+					length: length
+				}
+			});
+		} else {
+			populate(count, length);
+		}
+	});
 
-    procedureButton.addEventListener('click', () => {
-        worker.postMessage({
-            type: 'procedure'
-        });
-    });
+	procedureButton.addEventListener('click', () => {
+		if (asyncCheck.checked) {
+			worker.postMessage({
+				type: 'procedure'
+			});
+		} else {
+			const results = executeProcedure();
+			displayResults(results);
+		}
+	});
+
+	killButton.addEventListener('click', () => {
+		terminateAndCreateWorker();
+	});
 });
+
+const terminateAndCreateWorker = () => {
+	if (worker) {
+		worker.terminate();
+		console.log('worker terminated');
+	}
+	worker = new Worker('js/worker.js');
+};
 
 const MAX_RESULTS = 1000;
 const displayResults = (result) => {
-    while (output.firstChild)
-        output.removeChild(output.firstChild);
+	while (output.firstChild)
+		output.removeChild(output.firstChild);
 
-    const array = result.array;
-    const n = Math.min(array.length, MAX_RESULTS);
-    for (let i = 0; i < n; i++) {
-        const element = document.createElement('p');
-        element.innerHTML = array[i];
-        output.append(element);
-    }
+	const array = result.array;
+	const n = Math.min(array.length, MAX_RESULTS);
+	for (let i = 0; i < n; i++) {
+		const element = document.createElement('p');
+		element.innerHTML = array[i];
+		output.append(element);
+	}
 
-    const elapsed = result.time.toFixed(4);
-    if (result.async)
-        asyncTime.innerHTML = elapsed;
-    else
-        syncTime.innerHTML = elapsed;
+	const elapsed = result.time.toFixed(4);
+	if (result.async)
+		asyncTime.innerHTML = elapsed;
+	else
+		syncTime.innerHTML = elapsed;
 };
--- public/js/worker.js
+++ public/js/worker.js
@@ -1,71 +1,91 @@
 let arrayHolder = [];
+let cancelled = false;
 
-onmessage = function (event) {
-    const
-        type = event.data.type,
-        payload = event.data.payload;
+onmessage = function(event) {
+	const
+		type = event.data.type,
+		payload = event.data.payload;
 
-    let result = {};
-    switch (type) {
-        case 'populate':
-            populate(payload.count, payload.length);
-            break;
+	let result = {};
+	switch (type) {
+		case 'populate':
+			populate(payload.count, payload.length);
+			break;
 
-        case 'procedure':
-            const start = performance.now();
-            const array = procedure();
-            const elapsed = performance.now() - start;
-            result = {
-                async: true,
-                array: array,
-                time: elapsed,
-            };
-            break;
-    }
-    postMessage({
-        type: type,
-        payload: result
-    });
+		case 'procedure':
+			result = executeProcedure();
+			break;
+
+		case 'cancel':
+			cancelled = true;
+			break;
+	}
+	postMessage({
+		type: type,
+		payload: result
+	});
 };
 
 // TODO Make postMessage invocations for interval prints to inform interface about progress.
 const PRINT_INTERVAL = 1000;
 const populate = (count, length) => {
-    arrayHolder = [];
-    console.log('Running populate');
-    for (let i = 0; i <= count; i++) {
+	arrayHolder = [];
+	console.log('Running populate');
+	for (let i = 0; i < count; i++) {
 
-        if (i > 0 && i % PRINT_INTERVAL === 0)
-            console.log('Created ' + i + ' arrays');
+		if (cancelled)
+			return cancel();
 
-        const array = [];
-        for (let j = 0; j <= length; j++) {
-            const value = Math.random() * 9000 + 1000;
-            array.push(parseInt(value));
-        }
-        arrayHolder.push(array);
-    }
-    console.log('Populated arrays', arrayHolder);
-    return arrayHolder;
+		if (i > 0 && i % PRINT_INTERVAL === 0)
+			console.log('Created ' + i + ' arrays');
+
+		const array = [];
+		for (let j = 0; j < length; j++) {
+			const value = Math.random() * 9000 + 1000;
+			array.push(parseInt(value));
+		}
+		arrayHolder.push(array);
+	}
+	console.log('Populated arrays', arrayHolder);
+};
+
+const executeProcedure = () => {
+	const start = performance.now();
+	const array = procedure();
+	const elapsed = performance.now() - start;
+	return {
+		async: true,
+		array: array,
+		time: elapsed,
+	};
 };
 
 const procedure = () => {
-    console.log('Running adding procedure');
-    const sumArray = [];
-    for (let i = 0; i < arrayHolder.length; i++) {
+	console.log('Running adding procedure');
+	const sumArray = [];
+	for (let i = 0; i < arrayHolder.length; i++) {
 
-        if (i > 0 && i % PRINT_INTERVAL === 0)
-            console.log('Accumulated ' + i + ' arrays');
+		if (i > 0 && i % PRINT_INTERVAL === 0)
+			console.log('Accumulated ' + i + ' arrays');
 
-        const array = arrayHolder[i];
-        for (let j = 0; j < array.length; j++) {
-            const value = array[j];
-            if (sumArray[j])
-                sumArray[j] += value;
-            else
-                sumArray[j] = value;
-        }
-    }
-    console.log('Finished adding array values');
-    return sumArray;
+		const array = arrayHolder[i];
+		for (let j = 0; j < array.length; j++) {
+			const value = array[j];
+			if (sumArray[j])
+				sumArray[j] += value;
+			else
+				sumArray[j] = value;
+		}
+	}
+	console.log('Finished adding array values');
+	return sumArray;
+};
+
+const cancel = () => {
+	postMessage({
+		type: 'cancel'
+	});
+	setTimeout(function() {
+		cancelled = false;
+	}, 1000);
 };
